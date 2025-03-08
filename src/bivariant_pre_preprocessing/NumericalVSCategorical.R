1fa01207836090702734da4c9dcf89f2506ae2e6
Yasin <muhammad.yasin.khokhar@estudiantat.upc.edu>
2025-03-08 23:44:05 +0100

    Update type of variable year to categorical & save images in correct folder.

--- src/bivariant_pre_preprocessing/NumericalVSCategorical.R
+++ src/bivariant_pre_preprocessing/NumericalVSCategorical.R
@@ -1,18 +1,26 @@
-if (!dir.exists("Resultados")) dir.create("Resultados")
-datos <- read.csv("dataset.csv", header = TRUE, stringsAsFactors = FALSE)
+# To successfully run this script, set the working directory to data-mining-p1
+setwd(file.path(getwd(), "src/bivariant_pre_preprocessing"))
+
+datos <- read.csv("../../data/reduced_data.csv", header = TRUE, stringsAsFactors = FALSE)
 columnas_interes <- c("Month", "Year", "Customer_Age", "Customer_Gender", 
                       "Country", "Product_Category", "Order_Quantity", 
                       "Unit_Cost", "Unit_Price", "Size", "Color", 
                       "Warranty", "Shipping_Weight", "Delivery_Time", 
                       "Discount", "Eco_Friendly", "Shipping_Cost", "Insurance")
 datos <- datos[, columnas_interes]
-vars_categoricas <- c("Month", "Customer_Gender", "Country", "Product_Category",
+vars_categoricas <- c("Year", "Month", "Customer_Gender", "Country", "Product_Category",
                       "Size", "Color", "Warranty", "Eco_Friendly", "Insurance")
 datos[vars_categoricas] <- lapply(datos[vars_categoricas], as.factor)
 vars_numericas <- setdiff(columnas_interes, vars_categoricas)
 datos[vars_numericas] <- lapply(datos[vars_numericas], as.numeric)
 
-pdf("Resultados/boxplots.pdf", width = 8, height = 8)
+output_dir <- "../../images/before_preprocessing/bivariate/numericalVScategorical/"
+if (!dir.exists(output_dir)) {
+  print(paste("Creating directory", output_dir))
+  dir.create(output_dir, recursive = TRUE)
+}
+
+pdf(paste(output_dir, "boxplots.pdf", sep = ""), width = 8, height = 8)
 for (cat in vars_categoricas) {
   for (num in vars_numericas) {
     boxplot(as.formula(paste(num, "~", cat)), data = datos, col = "lightgreen",
@@ -22,7 +30,7 @@ for (cat in vars_categoricas) {
 dev.off()
 
 library(ggplot2)
-pdf("Resultados/histogramas.pdf", width = 8, height = 8)
+pdf(paste(output_dir, "histograms.pdf", sep = ""), width = 8, height = 8)
 for (cat in vars_categoricas) {
   for (num in vars_numericas) {
     print(ggplot(datos, aes_string(x = num, fill = cat)) +
