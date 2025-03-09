846ee1cf3ef6005f5f2be05d5b1a7648fee925d7
Yasin <muhammad.yasin.khokhar@estudiantat.upc.edu>
2025-03-09 01:30:50 +0100

    Add new feature year in categoricalvscategorial & generate pdf for images

--- src/bivariant_pre_preprocessing/CategoricalVSCategoricalAfterPreprocess.r
+++ src/bivariant_pre_preprocessing/CategoricalVSCategoricalAfterPreprocess.r
@@ -1,3 +1,6 @@
+# In this script we will implement the analysis of the relationship between categorical variables. 
+# We will use the contingency tables and multiple barplots to visualize the relationship between the variables.
+
 # Load libraries
 library(ggplot2)
 library(gridExtra)
@@ -9,87 +12,76 @@ setwd(file.path(getwd(), "/src/bivariant_pre_preprocessing"))
 # Read the data file
 data <- read.csv("../../data/filtered_data.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
 
-categoricalVariablesNames <- c("Month", "CustGender", "Country", "ProdCat", "Size", "Color", "Warranty", "EcoFriendly", "Insurance")
+data$Year <- as.factor(data$Year)
+# Set Month to print in the correct order, the months are in short format
+data$Month <- factor(datos$Month, levels = month.abb, ordered = TRUE)
+
+categoricalVariablesNames <- c("Year", "Month", "CustGender", "Country", "ProdCat", "Size", "Color", "Warranty", "EcoFriendly", "Insurance")
 # Select the categorical variables from the data and convert them to factors
 categoricalVariables <- lapply(data[,categoricalVariablesNames], as.factor)
 
 print(length(categoricalVariables))
 print(categoricalVariables)
 
-output_dir <- "../../images/after_preprocessing/bivariate/categoricalVScategorical/contingency_tables/"
+output_dir <- "../../images/after_preprocessing/bivariate/categoricalVScategorical/"
 
 if (!dir.exists(output_dir)) {
   print(paste("Creating directory", output_dir))
   dir.create(output_dir, recursive = TRUE)
 }
 
-contingency_tables <- list()
+pdf(file = paste(output_dir, "contingency_tables.pdf", sep = ""), width = 12, height = 7)
 
-# Loop through each pair of categorical variables
 for (i in 1:length(categoricalVariablesNames)) {
   for (j in 1:length(categoricalVariablesNames)) {
     if (i != j) {
       var1 <- categoricalVariables[[i]]
       var2 <- categoricalVariables[[j]]
-
+      
       contingencyTable <- table(var1, var2)
       contingencyTable <- cbind(contingencyTable, Total = rowSums(contingencyTable))
       contingencyTable <- rbind(contingencyTable, Total = colSums(contingencyTable))
-
-      # Print the contingency table
+      
       print(paste("Contingency table for", categoricalVariablesNames[i], "and", categoricalVariablesNames[j]))
       print(contingencyTable)
-
-      # Save the images in the graphics folder
-      png(file = paste(output_dir, categoricalVariablesNames[i], "_vs_", categoricalVariablesNames[j], ".png", sep = ""), width = 800, height = 600)
-
+      
       df <- as.data.frame.matrix(contingencyTable)
 
+      grid.newpage()
+
       grid.table(df)
-
-      grid.text(paste("Contingency table for", categoricalVariablesNames[i], "and", categoricalVariablesNames[j]), x = unit(0.5, "npc"), y = unit(0.95, "npc"), just = "center", gp = gpar(fontsize = 14, fontface = "bold"))
-
-      dev.off()
+      grid.text(paste("Contingency table for", categoricalVariablesNames[i], "and", categoricalVariablesNames[j]),
+                x = unit(0.5, "npc"), y = unit(0.95, "npc"), just = "center", 
+                gp = gpar(fontsize = 14, fontface = "bold"))
     }
   }
 }
+dev.off()
 
-print("Contingency tables have been created")
+print("Contingency tables have been saved to PDF.")
 
-output_dir <- "../../images/after_preprocessing/bivariate/categoricalVScategorical/multipleBarplots/"
-
-if (!dir.exists(output_dir)) {
-  print(paste("Creating directory", output_dir))
-  dir.create(output_dir, recursive = TRUE)
-}
-
+pdf(file = paste(output_dir, "multiple_barplots.pdf", sep = ""), width = 10, height = 8)
 
 for (i in 1:length(categoricalVariablesNames)) {
   for (j in 1:length(categoricalVariablesNames)) {
     if (i != j) {
       var1_name <- categoricalVariablesNames[i]
       var2_name <- categoricalVariablesNames[j]
-      
-      print(paste("Creating barplot for", var1_name, "and", var2_name))
-      
-      p <- ggplot(data, aes_string(x = var1_name, fill = var2_name)) +
-        geom_bar(position = "dodge") +
-        labs(title = paste("Multiple Barplot of", var1_name, "vs", var2_name),
-             x = var1_name,
-             y = "Count") +
-        theme_minimal() +
-        theme(plot.background = element_rect(fill = "white", colour = "white"),
-              legend.title = element_text(size = 10),
-              legend.text = element_text(size = 10),
-              plot.title = element_text(size = 14, face = "bold"),
-              axis.title = element_text(size = 12),
-              panel.border = element_blank())  
-      
-      ggsave(filename = paste(output_dir,"MultipleBarplot", var1_name, "_vs_", var2_name, ".png", sep = ""), plot = p, width = 10, height = 6)
+
+      print(ggplot(data, aes_string(x = var1_name, fill = var2_name)) +
+            geom_bar(position = "dodge") +
+            labs(x = var1_name, y = "Count") +
+            theme_minimal() +
+            theme(legend.title = element_text(size = 8),
+                  legend.text = element_text(size = 8),
+                  plot.title = element_text(size = 14, face = "bold", hjust = 0.5)) +  # Center and bold the title
+            ggtitle(paste(var1_name, "vs", var2_name))
+      )
     }
   }
 }
-print("Multiple barplots have been created")
 
+dev.off()
+
+print("Multiple barplots have been saved to PDF.")
 print("End of the script")
-
