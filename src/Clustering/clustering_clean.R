c6a40f77678d4c9288a0fe9e3b54542479450bd3
shintomf <146857839+shintomf@users.noreply.github.com>
2025-03-15 18:39:43 +0100

    Añadir mas cosas al codigo del clustering

--- src/Clustering/clustering_clean.R
+++ src/Clustering/clustering_clean.R
@@ -1,4 +1,8 @@
-# Cargar los datos
+# ------------------------------
+# Cargar y preprocesar los datos
+# ------------------------------
+setwd("C:/Users/usuario/Documents/uni_upc/ultimoaño/MD/scripts_r")
+
 dd <- read.csv("filtered_data.csv", sep = ",", stringsAsFactors = TRUE)
 
 # Definir las variables de interés
@@ -10,22 +14,70 @@ columnas_interes <- c("Month", "Year", "CustAge", "CustGender",
 vars_categoricas <- c("Year", "Month", "CustGender", "Country", "ProdCat",
                       "Size", "Color", "Warranty", "EcoFriendly", "Insurance")
 
-# Convertir variables categóricas y numéricas
+# Convertir variables a sus respectivos tipos
 dd[vars_categoricas] <- lapply(dd[vars_categoricas], as.factor)
 vars_numericas <- setdiff(columnas_interes, vars_categoricas)
 dd[vars_numericas] <- lapply(dd[vars_numericas], as.numeric)
-dcon <- data.frame(dd[vars_numericas])
+dcon <- data.frame(dd[vars_numericas])  # Solo las variables numéricas
 
-# Dendrograma 1: Euclidean Ward
-d_euclidean <- dist(dcon)  # Distancia Euclidiana
-h_euclidean <- hclust(d_euclidean, method = "ward.D2")
+# -------------------------------------------------------
+# Dendrograma 1: Euclidean Distance with Ward’s Method
+# -------------------------------------------------------
+d_euclidean <- dist(dcon)  # Calcula la matriz de distancias Euclidianas
+h_euclidean <- hclust(d_euclidean, method = "ward.D2")  # Clustering jerárquico con Ward
 plot(h_euclidean, main = "1. Euclidean ward")
 
-# Dendrograma 2: Gower Ward
+# ---------------------------------------------------------
+# Dendrograma 2: Gower Dissimilarity (Squared) with Ward’s Method
+# ---------------------------------------------------------
 library(cluster)
-# Seleccionamos columnas (ajusta 'actives' según corresponda)
+# 'actives' define las columnas a usar (aquí se incluyen tanto numéricas como categóricas)
 actives <- c(2:16)
-dissimMatrix <- daisy(dd[, actives], metric = "gower", stand = TRUE)
-distMatrix <- dissimMatrix^2  # Elevar al cuadrado para Ward
-h_gower <- hclust(distMatrix, method = "ward.D2")
+dissimMatrix <- daisy(dd[, actives], metric = "gower", stand = TRUE)  # Calcula la disimilitud con Gower
+distMatrix <- dissimMatrix^2  # Eleva al cuadrado para adecuarlo a Ward’s method
+h_gower <- hclust(distMatrix, method = "ward.D2")  # Clustering jerárquico con Ward
 plot(h_gower, main = "2. Gower ward")
+
+# ---------------------------------------------------------
+# Sección 1: Elbow Method (Método del Codo) con k-means
+# ---------------------------------------------------------
+# Se utiliza el dataset numérico (dcon)
+wss <- numeric(10)  # Vector para almacenar la suma total de cuadrados intra-cluster
+for (k in 1:10) {
+  km <- kmeans(dcon, centers = k)
+  wss[k] <- km$tot.withinss
+}
+plot(1:10, wss, type = "b", 
+     xlab = "Number of clusters", 
+     ylab = "Within-cluster sum of squares", 
+     main = "Elbow Method")
+
+# ---------------------------------------------------------
+# Sección 2: Silhouette Method (Método de la Silueta) con k-means
+# ---------------------------------------------------------
+library(cluster)
+sil_width <- numeric(9)
+for (k in 2:10) {
+  km <- kmeans(dcon, centers = k)
+  ss <- silhouette(km$cluster, dist(dcon))
+  sil_width[k-1] <- mean(ss[, 3])
+}
+plot(2:10, sil_width, type = "b", 
+     xlab = "Number of clusters", 
+     ylab = "Average silhouette width", 
+     main = "Silhouette Method")
+
+# ---------------------------------------------------------
+# Sección 3: Calinski-Harabasz Index con k-means
+# ---------------------------------------------------------
+install.packages("fpc")
+library(fpc)  # Asegúrate de tener instalado el paquete fpc
+ch_index <- numeric(9)
+for (k in 2:10) {
+  km <- kmeans(dcon, centers = k)
+  ch_index[k-1] <- calinhara(dcon, km$cluster, k)
+}
+plot(2:10, ch_index, type = "b", 
+     xlab = "Number of clusters", 
+     ylab = "Calinski-Harabasz Index", 
+     main = "Calinski-Harabasz Index")
