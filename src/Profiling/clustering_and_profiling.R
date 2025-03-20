cb1f71e53e4ca8b76b66eb2f760a85150bc5a67b
shintomf <146857839+shintomf@users.noreply.github.com>
2025-03-20 17:48:41 +0100

    Add files via upload

--- src/Profiling/clustering_and_profiling.R
+++ src/Profiling/clustering_and_profiling.R
@@ -1,18 +1,8 @@
-###############################################################################
-# SCRIPT DE CLUSTERING (GOWER + WARD) + PROFILING (4 CLUSTERS)
-###############################################################################
-
 # ------------------------------
-# 1. CONFIGURACIÓN INICIAL
+# Cargar y preprocesar los datos
 # ------------------------------
 setwd("C:/Users/usuario/Documents/uni_upc/ultimoaño/MD/scripts_r")
 
-# Cargar librerías necesarias
-library(cluster)   # Para daisy(), silhouette, etc.
-library(fpc)       # Para calinhara(), si la usas
-library(dplyr)     # Para manipulación de datos (opcional)
-
-# Cargar datos
 dd <- read.csv("filtered_data.csv", sep = ",", stringsAsFactors = TRUE)
 
 # Definir las variables de interés
@@ -24,42 +14,213 @@ columnas_interes <- c("Month", "Year", "CustAge", "CustGender",
 vars_categoricas <- c("Year", "Month", "CustGender", "Country", "ProdCat",
                       "Size", "Color", "Warranty", "EcoFriendly", "Insurance")
 
-# Convertir variables
+# Convertir variables a sus respectivos tipos
 dd[vars_categoricas] <- lapply(dd[vars_categoricas], as.factor)
 vars_numericas <- setdiff(columnas_interes, vars_categoricas)
 dd[vars_numericas] <- lapply(dd[vars_numericas], as.numeric)
+dcon <- data.frame(dd[vars_numericas])  # Solo las variables numéricas
 
-# Data frame solo con numéricas (para algunos análisis)
-dcon <- dd[vars_numericas]
+# -------------------------------------------------------
+# Dendrograma 1: Euclidean Distance with Ward’s Method
+# -------------------------------------------------------
+d_euclidean <- dist(dcon)  # Calcula la matriz de distancias Euclidianas
+h_euclidean <- hclust(d_euclidean, method = "ward.D2")  # Clustering jerárquico con Ward
+plot(h_euclidean, main = "1. Euclidean ward")
 
-# ------------------------------
-# 2. CLUSTERING: GOWER + WARD
-# ------------------------------
-dissimMatrix <- daisy(dd, metric = "gower", stand = TRUE)
-distMatrix <- dissimMatrix^2  
-h_gower <- hclust(distMatrix, method = "ward.D2")
+# ---------------------------------------------------------
+# Dendrograma 2: Gower Dissimilarity (Squared) with Ward’s Method
+# ---------------------------------------------------------
+library(cluster)
+# 'actives' define las columnas a usar (se incluyen tanto numéricas como categóricas)
+actives <- c(1:16)
+dissimMatrix <- daisy(dd[, actives], metric = "gower", stand = TRUE)  # Calcula la disimilitud con Gower
+distMatrix <- dissimMatrix^2  # Eleva al cuadrado para adecuarlo a Ward’s method
+h_gower <- hclust(distMatrix, method = "ward.D2")  # Clustering jerárquico con Ward
+plot(h_gower, main = "2. Gower ward")
 
-# Graficar el dendrograma de Gower
-plot(h_gower, main = "Gower ward dendrogram")
+# ---------------------------------------------------------
+# Creación de objetos de clusters para profiling:
+# Se crea 'c1' a partir del dendrograma Euclidean y 'c2' a partir del Gower
+# Ajusta k según lo que resulte más adecuado en tu análisis.
+# Por ejemplo, cortamos el dendrograma Euclidean en 2 clusters y el Gower en 4 clusters.
+#c1 <- cutree(h_euclidean, k = 2)
+c1 <- cutree(h_gower, k = 4)
 
-# Elegir número de clusters = 4
-k_final <- 4
-P <- cutree(h_gower, k_final)  # P es la variable de clase (clusters)
+##PROFILING##
 
-# Crear carpeta para guardar gráficos
+# Calcula els valor test de la variable Xnum per totes les modalitats del factor P
+ValorTestXnum <- function(Xnum,P){
+  nk <- as.vector(table(P))
+  n <- sum(nk)
+  xk <- tapply(Xnum,P,mean)
+  txk <- (xk - mean(Xnum)) / (sd(Xnum) * sqrt((n - nk)/(n * nk)))
+  pxk <- pt(txk, n - 1, lower.tail = FALSE)
+  for(c in 1:length(levels(as.factor(P)))){
+    if(pxk[c] > 0.5){ 
+      pxk[c] <- 1 - pxk[c]
+    }
+  }
+  return(pxk)
+}
+
+ValorTestXquali <- function(P,Xquali){
+  taula <- table(P, Xquali)
+  n <- sum(taula)
+  pk <- apply(taula, 1, sum) / n
+  pj <- apply(taula, 2, sum) / n
+  pf <- taula / (n * pk)
+  pjm <- matrix(data = pj, nrow = dim(pf)[1], ncol = dim(pf)[2], byrow = TRUE)
+  dpf <- pf - pjm
+  dvt <- sqrt(((1 - pk)/(n * pk)) %*% t(pj * (1 - pj)))
+  zkj <- dpf
+  zkj[dpf != 0] <- dpf[dpf != 0] / dvt[dpf != 0]
+  pzkj <- pnorm(zkj, lower.tail = FALSE)
+  for(c in 1:length(levels(as.factor(P)))){
+    for(s in 1:length(levels(Xquali))){
+      if(pzkj[c, s] > 0.5){ pzkj[c, s] <- 1 - pzkj[c, s] }
+    }
+  }
+  return(list(rowpf = pf, vtest = zkj, pval = pzkj))
+}
+
+# P contains class variable (which cluster an individual belongs to)
+P <- c1
+nameP <- "classe"
+
+nc <- length(levels(factor(P)))
+nc
+K <- 18
+pvalk <- matrix(data = 0, nrow = nc, ncol = K, dimnames = list(levels(P), names(dd)))
+nameP <- "Class"
+n <- dim(dd)[1]
+
 if(!dir.exists("profilingImages")) dir.create("profilingImages")
-outPath <- file.path(getwd(), "profilingImages")
+setwd("C:/Users/usuario/Documents/uni_upc/ultimoaño/MD/scripts_r/profilingImages")
 
-# ------------------------------
-# 3. FUNCIONES AUXILIARES (opcionales)
-# ------------------------------
+for(k in 1:K){
+  if(is.numeric(dd[, k])){
+    print(paste("Anàlisi per classes de la Variable:", names(dd)[k]))
+    
+    # Guardar Boxplot
+    png(paste("Boxplot", names(dd)[k], "Vs", nameP, ".png", sep = ""))
+    boxplot(dd[, k] ~ P, 
+            main = paste("Boxplot of", names(dd)[k], "vs", nameP),
+            horizontal = TRUE, xlab = names(dd)[k], ylab = nameP)
+    dev.off()
+    
+    # Guardar Barplot de Medias
+    png(paste("Means", names(dd)[k], "By", nameP, ".png", sep = ""))
+    barplot(tapply(dd[[k]], P, mean), 
+            main = paste("Means of", names(dd)[k], "by", nameP))
+    dev.off()
+    
+    abline(h = mean(dd[[k]]))
+    legend(0, mean(dd[[k]]), "global mean", bty = "n")
+    print("Estadístics per groups:")
+    for(s in levels(as.factor(P))) {
+      print(summary(dd[P == s, k]))
+    }
+    o <- oneway.test(dd[, k] ~ P)
+    print(paste("p-valueANOVA:", o$p.value))
+    kw <- kruskal.test(dd[, k] ~ P)
+    print(paste("p-value Kruskal-Wallis:", kw$p.value))
+    pvalk[, k] <- ValorTestXnum(dd[, k], P)
+    print("p-values ValorsTest: ")
+    print(pvalk[, k])
+    
+  } else {
+    if(class(dd[, k]) == "Date"){
+      print(summary(dd[, k]))
+      print(sd(dd[, k]))
+      png(paste("Hist_Date_", names(dd)[k], ".png", sep = ""), width = 800, height = 600)
+      hist(dd[, k], breaks = "weeks")
+      dev.off()
+    } else {
+      # Variables cualitatives
+      print(paste("Variable", names(dd)[k]))
+      tbl <- table(P, dd[, k])
+      rowperc <- prop.table(tbl, 1)
+      colperc <- prop.table(tbl, 2)
+      
+      dd[, k] <- as.factor(dd[, k])
+      
+      marg <- table(as.factor(P)) / n
+      print(append("Categories=", levels(as.factor(dd[, k]))))
+      
+      # Plot 1: Líneas de colperc
+      png(paste("Prop_lines_colperc_", names(dd)[k], ".png", sep = ""), width = 800, height = 600)
+      plot(marg, type = "l", ylim = c(0, 1), main = paste("Prop. of pos & neg by", names(dd)[k]))
+      paleta <- rainbow(length(levels(dd[, k])))
+      for(c in 1:length(levels(dd[, k]))){
+        lines(colperc[, c], col = paleta[c])
+      }
+      dev.off()
+      
+      # Plot 2: Líneas de colperc con leyenda
+      png(paste("Prop_lines_colperc_leg_", names(dd)[k], ".png", sep = ""), width = 800, height = 600)
+      plot(marg, type = "l", ylim = c(0, 1), main = paste("Prop. of pos & neg by", names(dd)[k]))
+      paleta <- rainbow(length(levels(dd[, k])))
+      for(c in 1:length(levels(dd[, k]))){
+        lines(colperc[, c], col = paleta[c])
+      }
+      legend("topright", levels(dd[, k]), col = paleta, lty = 2, cex = 0.6)
+      dev.off()
+      
+      # Plot 3: Líneas de rowperc
+      png(paste("Prop_lines_rowperc_", names(dd)[k], ".png", sep = ""), width = 800, height = 600)
+      plot(marg, type = "n", ylim = c(0, 1), main = paste("Prop. of pos & neg by", names(dd)[k]))
+      paleta <- rainbow(length(levels(dd[, k])))
+      for(c in 1:length(levels(dd[, k]))){
+        lines(rowperc[, c], col = paleta[c])
+      }
+      dev.off()
+      
+      # Plot 4: Líneas de rowperc con leyenda
+      png(paste("Prop_lines_rowperc_leg_", names(dd)[k], ".png", sep = ""), width = 800, height = 600)
+      plot(marg, type = "n", ylim = c(0, 1), main = paste("Prop. of pos & neg by", names(dd)[k]))
+      paleta <- rainbow(length(levels(dd[, k])))
+      for(c in 1:length(levels(dd[, k]))){
+        lines(rowperc[, c], col = paleta[c])
+      }
+      legend("topright", levels(dd[, k]), col = paleta, lty = 2, cex = 0.6)
+      dev.off()
+      
+      # Plot 5: Barplot apilat
+      png(paste("Barplot_stacked_", names(dd)[k], ".png", sep = ""), width = 800, height = 600)
+      paleta <- rainbow(length(levels(dd[, k])))
+      barplot(table(dd[, k], as.factor(P)), beside = FALSE, col = paleta)
+      legend("topright", levels(as.factor(dd[, k])), pch = 1, cex = 0.5, col = paleta)
+      dev.off()
+      
+      # Plot 6: Barplot adosat
+      png(paste("Barplot_sidebyside_", names(dd)[k], ".png", sep = ""), width = 800, height = 600)
+      paleta <- rainbow(length(levels(dd[, k])))
+      barplot(table(dd[, k], as.factor(P)), beside = TRUE, col = paleta)
+      legend("topright", levels(as.factor(dd[, k])), pch = 1, cex = 0.5, col = paleta)
+      dev.off()
+      
+      print("Test Chi quadrat: ")
+      print(chisq.test(dd[, k], as.factor(P)))
+      
+      print("valorsTest:")
+      print(ValorTestXquali(P, dd[, k]))
+    }
+  }
+}#endfor
 
-# Ejemplo de Snake Plot (opcional)
+for (c in 1:length(levels(as.factor(P)))) {
+  if(!is.na(levels(as.factor(P))[c])){
+    print(paste("P.values per class:", levels(as.factor(P))[c]))
+    print(sort(pvalk[c, ]), digits = 3)
+  }
+}
+
+# Afegeix el snake plot (nou)
 snake_plot <- function(data, clusters, file_name) {
   means <- aggregate(data, by = list(cluster = clusters), FUN = mean)
-  rownames(means) <- means[,1]
-  means <- means[,-1]
-  s_means <- scale(means)  # Estandarizar
+  rownames(means) <- means[, 1]
+  means <- means[ , -1]
+  s_means <- scale(means)
   png(file_name, width = 900, height = 600)
   matplot(t(s_means), type = "l", lty = 1, lwd = 2,
           xlab = "Variables", ylab = "Standardized Mean",
@@ -69,86 +230,4 @@ snake_plot <- function(data, clusters, file_name) {
   dev.off()
 }
 
-# ------------------------------
-# 4. PROFILING
-# ------------------------------
-
-# 4.1 "¿Son todas las variables importantes?"
-# - Numéricas: ANOVA + Kruskal-Wallis
-# - Categóricas: Chi-cuadrado
-for (varName in colnames(dd)) {
-  
-  png_file <- file.path(outPath, paste0("Profiling_", varName, ".png"))
-  
-  if(is.numeric(dd[[varName]])) {
-    # ANOVA y Kruskal-Wallis
-    anova_res <- oneway.test(dd[[varName]] ~ P)  # ANOVA
-    kw_res <- kruskal.test(dd[[varName]] ~ P)    # Kruskal-Wallis
-    
-    cat("\n-------------------------------------------\n")
-    cat("Variable numérica:", varName, "\n")
-    cat("ANOVA p-value:", anova_res$p.value, "\n")
-    cat("Kruskal-Wallis p-value:", kw_res$p.value, "\n")
-    
-    # Gráficos: Boxplot + Histograma global
-    png(png_file, width = 1000, height = 700)
-    par(mfrow = c(1,2))
-    boxplot(dd[[varName]] ~ P, 
-            main = paste("Boxplot of", varName, "by cluster"),
-            xlab = "Cluster", ylab = varName)
-    hist(dd[[varName]], main = paste("Histogram of", varName),
-         xlab = varName, col = "lightblue", breaks = 20)
-    dev.off()
-    
-  } else {
-    # Variable categórica
-    cat("\n-------------------------------------------\n")
-    cat("Variable categórica:", varName, "\n")
-    tbl <- table(P, dd[[varName]])
-    chi_res <- chisq.test(tbl)
-    cat("Chi-squared p-value:", chi_res$p.value, "\n")
-    
-    # Barplot side-by-side
-    png(png_file, width = 1000, height = 700)
-    barplot(tbl, beside = TRUE, legend = TRUE,
-            main = paste("Barplot of", varName, "by cluster"),
-            xlab = "Cluster", ylab = "Count", col = rainbow(nrow(tbl)))
-    dev.off()
-  }
-}
-
-# 4.2 "¿Qué diferencias hay entre clases?" (4 clusters)
-# - Numéricas: pairwise.t.test
-# - Categóricas: pairwise.prop.test
-vars_categoricas <- vars_categoricas[vars_categoricas %in% colnames(dd)]
-vars_numericas <- vars_numericas[vars_numericas %in% colnames(dd)]
-
-## 4.2.1 Pairwise T-tests (numéricas)
-cat("\n#### Pairwise T-tests para variables numéricas ####\n")
-for(numVar in vars_numericas) {
-  cat("\nVariable numérica:", numVar, "\n")
-  # pairwise.t.test recibe un vector de datos y un factor de grupos
-  ptt <- pairwise.t.test(dd[[numVar]], factor(P), p.adjust.method = "bonferroni")
-  print(ptt)
-}
-
-## 4.2.2 Pairwise prop.test (categóricas)
-cat("\n#### Pairwise prop.test para variables categóricas ####\n")
-for(catVar in vars_categoricas) {
-  cat("\nVariable categórica:", catVar, "\n")
-  
-  # Para cada categoría
-  catLevels <- levels(dd[[catVar]])
-  for(lvl in catLevels) {
-    cat("Categoría:", lvl, "\n")
-    x_counts <- tapply(dd[[catVar]] == lvl, P, sum)
-    n_counts <- table(P)
-    pwt <- pairwise.prop.test(x = x_counts, n = n_counts, p.adjust.method = "bonferroni")
-    print(pwt)
-  }
-}
-
-# 4.3 Snake Plot (opcional)
-snake_plot(dcon, P, file.path(outPath, "SnakePlot.png"))
-
-cat("\nFinalizado el profiling con 4 clusters (Gower). Revisa la carpeta:", outPath, "\n")
+snake_plot(dcon, P, "SnakePlot.png")
